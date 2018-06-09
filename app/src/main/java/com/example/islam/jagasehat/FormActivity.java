312f963a0ac44755210fa942da98c6002ff5ed01
muhamistau <islam.taufikurahman@gmail.com>
2018-06-10 01:21:19 +0700

    Form Page update and fix

--- app/src/main/java/com/example/islam/jagasehat/FormActivity.java
+++ app/src/main/java/com/example/islam/jagasehat/FormActivity.java
@@ -2,44 +2,101 @@ package com.example.islam.jagasehat;
 
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FormActivity extends AppCompatActivity {
 
-    String[] jenisKelamin = {"Laki-laki", "Perempuan"};
+    private DatabaseReference mDatabase;
+    TextInputLayout umurInputLayout;
+    TextInputEditText umur;
+    EditText email;
+    Spinner spinJK;
+    Spinner spinPendidikan;
+    Spinner spinPekerjaan;
+    CardView kirim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
-        Spinner spinJK = (Spinner) findViewById(R.id.spinnerJK);
+        umurInputLayout = (TextInputLayout) findViewById(R.id.umurTextInputlayout);
+        umur = (TextInputEditText) findViewById(R.id.umurEditText);
+
+        spinJK = (Spinner) findViewById(R.id.spinnerJK);
         ArrayAdapter<CharSequence> adapterJK = ArrayAdapter.createFromResource(this, R.array.jk_array, R.layout.spinner_item);
         adapterJK.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinJK.setAdapter(adapterJK);
 
-        Spinner spinPendidikan = (Spinner) findViewById(R.id.spinnerPendidikan);
+        spinPendidikan = (Spinner) findViewById(R.id.spinnerPendidikan);
         ArrayAdapter<CharSequence> adapterPendidikan = ArrayAdapter.createFromResource(this, R.array.pendidikan_array, R.layout.spinner_item);
         adapterPendidikan.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinPendidikan.setAdapter(adapterPendidikan);
 
-        Spinner spinPekerjaan = (Spinner) findViewById(R.id.spinnerPekerjaan);
+        spinPekerjaan = (Spinner) findViewById(R.id.spinnerPekerjaan);
         ArrayAdapter<CharSequence> adapterPekerjaan = ArrayAdapter.createFromResource(this, R.array.pekerjaan_array, R.layout.spinner_item);
         adapterPekerjaan.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinPekerjaan.setAdapter(adapterPekerjaan);
+
+        email = (EditText) findViewById(R.id.emailEditText);
+
+        kirim = (CardView) findViewById(R.id.cardkirim);
+
+        kirim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kirimData();
+            }
+        });
     }
 
-    void next (View view) {
 
-        Intent intent = new Intent(FormActivity.this, DaftarActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
+    void kirimData() {
+
+        if (TextUtils.isEmpty(umur.getText())) {
+
+            umurInputLayout.setError(getString(R.string.umur_error));
+            umurInputLayout.requestFocus();
+
+        } else {
+
+            mDatabase = FirebaseDatabase.getInstance().getReference();
+
+            //Getting Values
+            int umurAngka = Integer.parseInt(umur.getText().toString());
+            String jkText = spinJK.getSelectedItem().toString();
+            String pendidikanText = spinPendidikan.getSelectedItem().toString();
+            String pekerjaanText = spinPekerjaan.getSelectedItem().toString();
+            String emailText = email.getText().toString();
+
+            //Creating new user node
+            String userId = mDatabase.push().getKey();
+
+            //Creating user Object
+            User user = new User(umurAngka, jkText, pendidikanText, pekerjaanText, emailText);
+
+            //Pushing user to 'users node using userID
+            mDatabase.child("users").child(userId).setValue(user);
+
+            Intent intent = new Intent(FormActivity.this, DaftarActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
+        }
 
     }
+
 }
