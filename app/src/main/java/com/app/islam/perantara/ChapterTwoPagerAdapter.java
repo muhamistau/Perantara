46ebd69fc98cf979cc85e5c6aa9936f7495f12eb
muhamistau <islam.taufikurahman@gmail.com>
2019-01-08 15:19:34 +0700

    Update Layout pada Fragment Chapter 2 & 3

--- app/src/main/java/com/app/islam/perantara/ChapterTwoPagerAdapter.java
+++ app/src/main/java/com/app/islam/perantara/ChapterTwoPagerAdapter.java
@@ -3,6 +3,7 @@ package com.app.islam.perantara;
 import android.content.Context;
 
 import com.app.islam.perantara.chapter2.ChapterTwo1Fragment;
+import com.app.islam.perantara.chapter2.ChapterTwo21Fragment;
 import com.app.islam.perantara.chapter2.ChapterTwo2Fragment;
 import com.app.islam.perantara.chapter2.ChapterTwo3Fragment;
 import com.app.islam.perantara.chapter2.ChapterTwo4Fragment;
@@ -18,7 +19,7 @@ import androidx.fragment.app.FragmentPagerAdapter;
 
 public class ChapterTwoPagerAdapter extends FragmentPagerAdapter {
 
-    private final int PAGE_COUNT = 10; // Defining how many page in this chapter for the Viewpager
+    private final int PAGE_COUNT = 11; // Defining how many page in this chapter for the Viewpager
     private Context context;
 
     // Default Constructor for PagerAdapter
@@ -30,27 +31,54 @@ public class ChapterTwoPagerAdapter extends FragmentPagerAdapter {
     // Defining which fragment at which page
     @Override
     public Fragment getItem(int position) {
-        if (position == 0) {
-            return new ChapterWelcomeFragment();
-        } else if (position == 1) {
-            return new ChapterTwo1Fragment();
-        } else if (position == 2) {
-            return new ChapterTwo2Fragment();
-        } else if (position == 3) {
-            return new ChapterTwo3Fragment();
-        } else if (position == 4) {
-            return new ChapterTwo4Fragment();
-        } else if (position == 5) {
-            return new ChapterTwo5Fragment();
-        } else if (position == 6) {
-            return new ChapterTwo6Fragment();
-        } else if (position == 7) {
-            return new ChapterTwo7Fragment();
-        } else if (position == 8) {
-            return new ChapterTwo8Fragment();
-        } else {
-            return new ChapterTwoDoneFragment();
+        switch (position) {
+            case 0:
+                return new ChapterWelcomeFragment();
+            case 1:
+                return new ChapterTwo1Fragment();
+            case 2:
+                return new ChapterTwo2Fragment();
+            case 3:
+                return new ChapterTwo21Fragment();
+            case 4:
+                return new ChapterTwo3Fragment();
+            case 5:
+                return new ChapterTwo4Fragment();
+            case 6:
+                return new ChapterTwo5Fragment();
+            case 7:
+                return new ChapterTwo6Fragment();
+            case 8:
+                return new ChapterTwo7Fragment();
+            case 9:
+                return new ChapterTwo8Fragment();
+            default:
+                return new ChapterTwoDoneFragment();
+
         }
+//        if (position == 0) {
+//            return new ChapterWelcomeFragment();
+//        } else if (position == 1) {
+//            return new ChapterTwo1Fragment();
+//        } else if (position == 2) {
+//            return new ChapterTwo21Fragment();
+//        } else if (position == 3) {
+//            return new ChapterTwo2Fragment();
+//        } else if (position == 4) {
+//            return new ChapterTwo3Fragment();
+//        } else if (position == 5) {
+//            return new ChapterTwo4Fragment();
+//        } else if (position == 6) {
+//            return new ChapterTwo5Fragment();
+//        } else if (position == 7) {
+//            return new ChapterTwo6Fragment();
+//        } else if (position == 8) {
+//            return new ChapterTwo7Fragment();
+//        } else if (position == 9) {
+//            return new ChapterTwo8Fragment();
+//        } else {
+//            return new ChapterTwoDoneFragment();
+//        }
     }
 
     @Override
---
+++ app/src/main/java/com/app/islam/perantara/chapter2/ChapterTwo21Fragment.java
@@ -0,0 +1,31 @@
+package com.app.islam.perantara.chapter2;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.islam.perantara.R;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChapterTwo21Fragment extends Fragment {
+
+
+    public ChapterTwo21Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chapter_two21, container, false);
+    }
+
+}
